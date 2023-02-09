11e499042b1abc838702225787a388b79ffbb550
berkulutas <berkulutas2001@gmail.com>
2023-02-09 17:24:35 +0300

    two sum

--- cpp/1_two_sum.cpp
+++ cpp/1_two_sum.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        vector<int> res;
+        unordered_map<int, int> umap; 
+
+        for (int i = 0; i<nums.size(); i++) {
+            // cout << nums[i] << " " << target - nums[i] << "\n";
+            auto tmp = umap.find(target - nums[i]); 
+            if (tmp != umap.end()) {
+                // cout << "found " << nums[i] << " " <<(*tmp).first << "\n";
+                res.push_back(i);
+                res.push_back((*tmp).second);
+                break; 
+
+            }
+            else {
+                // cout << "else " << nums[i] << " " << i << "\n";
+                umap.insert(make_pair(nums[i], i));
+            }
+        }
+
+        return res; 
     }
 };
