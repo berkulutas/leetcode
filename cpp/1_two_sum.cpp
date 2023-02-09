ef63e899243e65ae436d2076d334a166b6d159ad
berkulutas <berkulutas2001@gmail.com>
2023-02-09 17:24:59 +0300

    two sum

--- cpp/1_two_sum.cpp
+++ cpp/1_two_sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+
+        // check element's complement in map
+
+        // if add indices to result arr, else add element and index to map
+
         vector<int> res;
         unordered_map<int, int> umap; 
 
         for (int i = 0; i<nums.size(); i++) {
-            // cout << nums[i] << " " << target - nums[i] << "\n";
             auto tmp = umap.find(target - nums[i]); 
-            if (tmp != umap.end()) {
-                // cout << "found " << nums[i] << " " <<(*tmp).first << "\n";
-                res.push_back(i);
-                res.push_back((*tmp).second);
+            
+            if (tmp != umap.end()) { // check element's complement in map
+                res.push_back(i); // current element's index
+                res.push_back((*tmp).second); // complement's index
                 break; 
 
             }
             else {
-                // cout << "else " << nums[i] << " " << i << "\n";
-                umap.insert(make_pair(nums[i], i));
+                umap.insert(make_pair(nums[i], i)); // add element with index
             }
         }
 
