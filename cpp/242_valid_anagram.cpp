ef3755626c3c14a26b5a98bdf6ed64f79f9bb0a4
berkulutas <berkulutas2001@gmail.com>
2023-02-14 19:39:27 +0300

    update problem 242

--- cpp/242_valid_anagram.cpp
+++ cpp/242_valid_anagram.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if (s == t) return true; 
-        else return false; 
+        if (s.size() != t.size()) return false;
+
+        vector<int> freqs(26,0);
+
+        for (int i =0; i<s.size(); i++) {
+            freqs[s[i]-'a']++;
+            freqs[t[i]-'a']--; 
+        }
+
+        for (auto &tmp : freqs) {
+            if (tmp != 0) return false; 
+        }
+        return true;
+
     }
 };
