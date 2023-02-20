1387fefd7f714826919df87ec155f04e7c71ee74
berkulutas <berkulutas2001@gmail.com>
2023-02-20 17:20:11 +0300

    update problem 238

--- cpp/238_product_of_array_except_self.cpp
+++ cpp/238_product_of_array_except_self.cpp
@@ -2,6 +2,31 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int size = nums.size();
+        vector<int> output(size,1);
+
+        int leftP = 1; 
+        for (int i = 1; i<size; i++) {
+            leftP *= nums[i-1];
+            output[i] *= leftP; 
+        }
+        int rightP = 1;
+        for (int i = size-2; i>=0; i--) {
+            rightP *= nums[i+1];
+            output[i] *= rightP; 
+        }
+        return output;
+    }
+};
+
+
+
+// compute leftProduct and rightProduct for every element in arr O(n).
+// ans[i] = leftProcut[i]*rightProduct[i] O(n)
+/*
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
         vector<int> leftProduct(size);
         vector<int> rightProduct(size);
         leftProduct[0] = 1;
@@ -18,4 +43,5 @@ public:
 
         return output;
     }
-};
+}; 
+*/
